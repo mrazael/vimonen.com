e9aa38741af366b069f5fc914bc83516f9e08c63
mrazael <99121626+mrazael@users.noreply.github.com>
2026-05-09 01:28:20 +0300

    Update posts.js

--- devlog/posts.js
+++ devlog/posts.js
@@ -14,5 +14,11 @@ const devlogPosts = [
         title: "Second post", 
         date: "May 7th, 2026",
         path: "2026/05/0001" 
+    },
+    { 
+        id: "0002", 
+        title: "IDE/DSL update", 
+        date: "May 8th, 2026",
+        path: "2026/05/0002" 
     }
 ];
